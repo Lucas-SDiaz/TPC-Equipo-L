021b21f4333e24bda4649b6aebea81c03ae41edd
ATILA2001 <nahuel.carracedo@alumnos.frgp.utn.edu.ar>
2024-07-10 23:44:45 -0300

    diseño cliente

--- TPC_Equipo_L/TPC_Equipo_L/modificarVentaS.aspx.designer.cs
+++ TPC_Equipo_L/TPC_Equipo_L/modificarVentaS.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace TPC_Equipo_L
         protected global::System.Web.UI.WebControls.TextBox txtSeguimiento;
 
         /// <summary>
-        /// Control btnModificar.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnModificar;
-
-        /// <summary>
         /// Control btnVolver.
         /// </summary>
         /// <remarks>
@@ -58,6 +49,15 @@ namespace TPC_Equipo_L
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnVolver;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
 
         /// <summary>
         /// Control lblMensaje.
